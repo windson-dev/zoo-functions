a13344f1bc7820c306289ed6f84a5c8937e3e163
Windson Macedo <windsondev@hotmail.com>
2022-07-04 15:07:36 -0300

    80% dos requisitos

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,17 @@
+const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const getEmployees = employees.find((element) => element.id === id);
+  const getAnimal = species.find((element) => element.id === getEmployees.responsibleFor[0]);
+
+  const getOldestAnimal = getAnimal.residents.sort((a, b) => b.age - a.age);
+
+  const [{ name, sex, age }] = getOldestAnimal;
+
+  return [name, sex, age];
 }
+
+console.log(getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
 
 module.exports = getOldestFromFirstSpecies;
